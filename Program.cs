ec5ba5eb1c7d1664788522803b7eaca2c82ad78e
Геннадий Матвиенко <gennady39@hotmail.com>
2023-03-13 18:07:21 +0300

    Решение задачи № 25

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// № 25 Напишите цикл, который принимает на вход
+//два числа(A и B) и возводит число A в натуральную степень B.
+
+/*
+
+void DegreeOfNumber(double arg1, double arg2)                // Первыый аргумент должен присваивать значения числа которое хотим возвести в степень
+{                                                    // Второе аргумент должен присваивать значение степени
+    double ans = Math.Pow(arg1, arg2);
+
+    Console.WriteLine("Число : ");
+    Console.Write(arg1);
+    Console.Write(" В степени : ");
+    Console.Write(arg2);
+    Console.Write(" = ");
+    Console.Write(ans);
+}
+
+Console.WriteLine("Введите число которое хотите возвести в степень");
+double A = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа");
+double B = double.Parse(Console.ReadLine());
+
+DegreeOfNumber(A, B);
+Console.ReadLine();
+
+*/
+
